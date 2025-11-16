797328378f48709c3ffac38982064ef21b8174c3
7682829 <arunimamehta01325@gmail.com>
2025-11-17 03:26:40 +0530

    added functionality to an admin panel button

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -3,6 +3,10 @@ import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Sidebar = () => {
+  const openWarehouse = () => {
+    window.open('/Wilhelmus.html', '_blank');
+  };
+
   return (
     <div className='w-[18%] min-h-screen border-r-2'>
         <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
@@ -21,15 +25,13 @@ const Sidebar = () => {
                 <p className='hidden md:block'>Orders</p>    
             </NavLink>
 
-            <a 
-                href="#" 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l transition-all duration-300 hover:bg-gray-100'
+            <button 
+                onClick={openWarehouse}
+                className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l transition-all duration-300 hover:bg-gray-100 cursor-pointer bg-transparent text-left'
             > 
                 <img className='w-5 h-5' src={assets.order_icon} alt=""/>
                 <p className='hidden md:block'>Go to Warehouse</p>    
-            </a>
+            </button>
         </div>
       
     </div>
